cc6073bfe682092304cd51c5333678fc9f55a964
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2022-11-18 20:26:02 +0900

    개발: 군복무계산기 추가

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,61 @@
 window.addEventListener("load", (function() {
-    var ko = document.getElementsByClassName("ko");
-    var en = document.getElementsByClassName("en");
-    var koreanSwitch = document.getElementById("koreanSwitch");
+    const ko = document.getElementsByClassName("ko");
+    const en = document.getElementsByClassName("en");
+    const koreanSwitch = document.getElementById("koreanSwitch");
+    const militaryRate = document.getElementById("military_rate");
+    const time_span = document.getElementById("current-local-time");
 
-    var time_span = document.getElementById("current-local-time");
+    const rank_span_ko = document.getElementById("rank-ko");
+    const rank_span_en = document.getElementById("rank-en");
+    const next_rank_span_ko = document.getElementById("next-rank-ko");
+    const next_rank_span_en = document.getElementById("next-rank-en");
+    const enlist_span = document.getElementById("enlist");
+    const discharge_span = document.getElementById("discharge");
+    const promotion_span = document.getElementById("promotion");
+    const enlist_date_span_ko = document.getElementById("enlist-date-ko");
+    const enlist_date_span_en = document.getElementById("enlist-date-en");
+    const discharge_date_span_ko = document.getElementById("discharge-date-ko");
+    const discharge_date_span_en = document.getElementById("discharge-date-en");
+    const promotion_date_span_ko = document.getElementById("promotion-date-ko");
+    const promotion_date_span_en = document.getElementById("promotion-date-en");
+
+    const c = 1000 * 60 * 60 * 24;
+    
+    const enlistDate = new Date(2022, 2, 28);
+    const dischargeDate = new Date(2023, 10, 27);
+    const promotions = [new Date(2022, 2, 28), new Date(2022, 5, 1),new Date(2022, 11, 1),new Date(2023, 5, 1), new Date(2023, 10, 27)];
+    const rank_ko = ['이병', '일병', '상병', '병장', '전역'];
+    const rank_en = ['Seaman Apprentice', 'Seaman', 'Petty Officer 3rd Class', 'Petty Officer 2nd Class', 'Discharge'];
+
+    function militaryService() {
+        var rate = ((new Date() - enlistDate)/(dischargeDate-enlistDate)*100).toFixed(7);
+
+        militaryRate.innerHTML = `${rate}%`;
+        militaryRate.style.width = `${rate}%`;
+        militaryRate.setAttribute('aria-valuenow', rate);
+    
+        for (let i=0; i<promotions.length; i++) {
+            if(new Date() <= promotions[i]) {
+                rank_span_ko.innerHTML = rank_ko[i-1];
+                rank_span_en.innerHTML = rank_en[i-1];
+                next_rank_span_ko.innerHTML = rank_ko[i];
+                next_rank_span_en.innerHTML = rank_en[i];
+                enlist_span.innerHTML = Math.ceil((new Date() - enlistDate)/c);
+                discharge_span.innerHTML = Math.floor((new Date() - dischargeDate)/c);
+                promotion_span.innerHTML = Math.floor((new Date() - promotions[i])/c);
+    
+                enlist_date_span_ko.innerHTML = enlistDate.toLocaleDateString('ko-KR', {year: 'numeric', month: 'long', day: 'numeric',});
+                enlist_date_span_en.innerHTML = enlistDate.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric',});
+                discharge_date_span_ko.innerHTML = dischargeDate.toLocaleDateString('ko-KR', {year: 'numeric', month: 'long', day: 'numeric',});
+                discharge_date_span_en.innerHTML = dischargeDate.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric',});
+                promotion_date_span_ko.innerHTML = promotions[i].toLocaleDateString('ko-KR', {year: 'numeric', month: 'long', day: 'numeric',});
+                promotion_date_span_en.innerHTML = promotions[i].toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric',});
+    
+                break;
+            }
+        }    
+    }
+
     function localTime() {
         // 1. 현재 시간(Locale)
         const curr = new Date();
@@ -22,8 +74,11 @@ window.addEventListener("load", (function() {
     }
     localTime();
     setInterval(localTime, 1000);
+    militaryService();
+    setInterval(militaryService, 100);
 
 
+    
     if (koreanSwitch) {
         initLang();
         koreanSwitch.addEventListener("change", (function() {

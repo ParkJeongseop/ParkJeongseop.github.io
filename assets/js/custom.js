54a4835a9c37f1364d2c549502b7f63cf5a48fee
ParkJeongseop <parkjeongseop@parkjeongseop.com>
2023-06-19 14:23:31 +0900

    수정: 전역일계산기 전역후 표시 수정

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,42 +18,62 @@ window.addEventListener("load", (function() {
     const discharge_date_span_en = document.getElementById("discharge-date-en");
     const promotion_date_span_ko = document.getElementById("promotion-date-ko");
     const promotion_date_span_en = document.getElementById("promotion-date-en");
+    const promotion_div = document.getElementById("promotion-div");
 
     const c = 1000 * 60 * 60 * 24;
     
     const enlistDate = new Date(2022, 2, 28);
     const dischargeDate = new Date(2023, 10, 27);
-    const promotions = [new Date(2022, 2, 28), new Date(2022, 5, 1),new Date(2022, 11, 1),new Date(2023, 5, 1), new Date(2023, 10, 27)];
+    const promotions = [new Date(2022, 2, 28), new Date(2022, 5, 1), new Date(2022, 11, 1), new Date(2023, 5, 1), new Date(2023, 10, 27)];
     const rank_ko = ['이병', '일병', '상병', '병장', '전역'];
     const rank_en = ['Seaman Apprentice', 'Seaman', 'Petty Officer 3rd Class', 'Petty Officer 2nd Class', 'Discharge'];
 
+    /**
+     * 양수값 +기호 표시
+     * @param {number} num - 입력 수
+     * @returns {string} 결과 문자열
+     */
+    function display_plus(num) {
+        return num >= 0 ? '+' + num : String(num);
+    }
+
+
+    /**
+     * 군 복무일 계산 함수
+     */
     function militaryService() {
         var rate = ((new Date() - enlistDate)/(dischargeDate-enlistDate)*100).toFixed(7);
+        rate = rate > 100 ? 100 : rate;
 
         militaryRate.innerHTML = `${rate}%`;
         militaryRate.style.width = `${rate}%`;
         militaryRate.setAttribute('aria-valuenow', rate);
-    
-        for (let i=0; i<promotions.length; i++) {
-            if(new Date() <= promotions[i]) {
+
+        for (let i=0; i < promotions.length; i++) {
+            if(new Date() <= promotions[i] || i == promotions.length-1) {
                 rank_span_ko.innerHTML = rank_ko[i-1];
                 rank_span_en.innerHTML = rank_en[i-1];
                 next_rank_span_ko.innerHTML = rank_ko[i];
                 next_rank_span_en.innerHTML = rank_en[i];
-                enlist_span.innerHTML = Math.ceil((new Date() - enlistDate)/c);
-                discharge_span.innerHTML = Math.floor((new Date() - dischargeDate)/c);
-                promotion_span.innerHTML = Math.floor((new Date() - promotions[i])/c);
+                enlist_span.innerHTML = display_plus(Math.ceil((new Date() - enlistDate)/c));
+                discharge_span.innerHTML = display_plus(Math.floor((new Date() - dischargeDate)/c));
+                promotion_span.innerHTML = display_plus(Math.floor((new Date() - promotions[i])/c));
     
                 enlist_date_span_ko.innerHTML = enlistDate.toLocaleDateString('ko-KR', {year: 'numeric', month: 'long', day: 'numeric',});
                 enlist_date_span_en.innerHTML = enlistDate.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric',});
                 discharge_date_span_ko.innerHTML = dischargeDate.toLocaleDateString('ko-KR', {year: 'numeric', month: 'long', day: 'numeric',});
                 discharge_date_span_en.innerHTML = dischargeDate.toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric',});
-                promotion_date_span_ko.innerHTML = promotions[i].toLocaleDateString('ko-KR', {year: 'numeric', month: 'long', day: 'numeric',});
-                promotion_date_span_en.innerHTML = promotions[i].toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric',});
-    
+
+                if(i < 4) {
+                    promotion_date_span_ko.innerHTML = promotions[i].toLocaleDateString('ko-KR', {year: 'numeric', month: 'long', day: 'numeric',});
+                    promotion_date_span_en.innerHTML = promotions[i].toLocaleDateString('en-US', {year: 'numeric', month: 'long', day: 'numeric',});
+                } else {
+                    promotion_div.style.display = 'none';
+                }
+
                 break;
             }
-        }    
+        }
     }
 
     function localTime() {
